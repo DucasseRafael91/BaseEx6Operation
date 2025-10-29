af84d12b19ff7943c025fdea03335cb6dafe555b
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-29 11:18:09 +0100

    Ajout de la méthode mul

--- src/baseEx6Operation/Operation.java
+++ src/baseEx6Operation/Operation.java
@@ -5,13 +5,16 @@ public class Operation {
 	public static int add(int operand_1, int operand_2) {
 		
 		return operand_1 + operand_2;
-		
 	}
 	
 	public static int sub(int operand_1, int operand_2) {
 		
 		return operand_1 - operand_2;
+	}
+	
+	public static int mul(int operand_1, int operand_2) {
 		
+		return operand_1 * operand_2;
 	}
 
 }
